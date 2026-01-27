cd9cd2f4a29ed923e3325f1a5da9bb7710483220
Devjack404 <juansabarofek@gmail.com>
2026-01-27 20:45:29 +0700

    Membuat function menambah operator penghitungan dan membuat function untuk menghitung

--- kalkulator/script.js
+++ kalkulator/script.js
@@ -18,3 +18,45 @@ function appendNumber(number){
     inputCurrent += number;
     updateDisplay();
 }
+
+function appendOperators (operator) {
+    if (inputCurrent === "") return;
+    inputPrevious = inputCurrent;
+    inputOperator = operator;
+    inputCurrent = "";
+    updateDisplay();
+    
+}
+
+
+function calculate(){
+    let prev = Number(inputPrevious);
+    let curr = Number(inputCurrent); 
+    let result;
+
+    switch (inputOperator) {
+        case "/":
+            if(curr === 0){
+                alert("Cannot divide by zero !!")
+                return;
+            }
+            result = prev / curr;
+            break;
+        case "+":
+            result = prev + curr;
+            break;
+        case "-":
+            result = prev - curr;
+            break;
+        case "X":
+            result = prev * curr;
+            break;
+        default :
+            return;
+    }
+
+    inputOperator = result;
+    inputPrevious = '';
+    inputCurrent = '';
+    updateDisplay();
+}
