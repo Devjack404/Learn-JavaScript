ed63604a70d126654085a54f0739073aa5c60dec
Devjack404 <juansabarofek@gmail.com>
2026-01-29 10:29:01 +0700

    wip : pop error ketika click

--- popup-box/script.js
+++ popup-box/script.js
@@ -1,6 +1,11 @@
-let popup_button = document.getElementById('popup_button');
+let open_button = document.getElementById('popup_button');
+
+let closed_button = document.getElementById('closed_button');
+
+let popup = document.querySelector('.popup'); 
 
 
-function popup_button() {
-    
-}
+open_button.addEventListener("click", () => {
+    popup.style.display = "block";
+    document.body.style.backgroundColor = "#9EA9B1";
+});
