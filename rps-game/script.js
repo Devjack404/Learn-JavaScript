859f7a047e986ac10f853ba5025375d9bc72ba17
Devjack404 <juansabarofek@gmail.com>
2026-01-31 17:49:44 +0700

    membuat variabel score dan membuat logika perbandingan nilai pilihan user

---
+++ rps-game/script.js
@@ -0,0 +1,16 @@
+let UserScore = document.querySelector('.userScore');
+let computerScore = document.querySelector('.computerScore');
+
+function choice (userChoice){
+ if(userChoice === "Rock") {
+    console.log("User memilih rock")
+ }
+ else if (userChoice === "Paper") {
+    console.log("User memilih paper")
+ }
+ else if(userChoice === "Scissors") {
+    console.log("User memilih scissors")
+ }
+
+}
+
