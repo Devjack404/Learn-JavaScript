f6ab4a70df237414715eeaf36e299bf286e58be9
Devjack404 <juansabarofek@gmail.com>
2026-02-02 15:30:49 +0700

    logic game

--- rps-game/script.js
+++ rps-game/script.js
@@ -1,18 +1,41 @@
-let UserScore = document.querySelector('.userScore');
-let computerScore = document.querySelector('.computerScore');
-let userChoice;
-  
-function Choice (userChoice){
- if(userChoice === "Rock") {
-    console.log("User memilih rock")
- }
- else if (userChoice === "Paper") {
-    console.log("User memilih paper")
- }
- else if(userChoice === "Scissors") {
-    console.log("User memilih scissors")
- }
+//varibel for value score
+let userScore = 0;
+let comScore = 0;
+
+//varibel for ever tag in html
+let UserScore_span = document.getElementById('userScore');
+let ComputerScore_span = document.getElementById('computerScore');
+
+//function to check who's the winner 
+function win(userChoice, comChoice) {
+  userScore++;
+  UserScore_span.innerHTML = userScore;
+  ComputerScore_span.innerHTML = comScore;
 }
+
+function lose(userChoice, comChoice) {
+  comScore++;
+  UserScore_span.innerHTML = userScore;
+  ComputerScore_span.innerHTML = comScore;
+}
+
+function draw(userChoice, comChoice) {
+  UserScore_span.innerHTML = userScore;
+  ComputerScore_span.innerHTML = comScore;
+}
+
+function play(userChoice){
+  const ComChoice = computerChoice(); 
+  switch (userChoice + ComChoice) {
+    case value:
+      
+      break;
+
+    default:
+      break;
+  }
+}
+
 
 
 //function computer play
@@ -21,5 +44,4 @@ function computerChoice (){
   const randomChoice = Math.floor(Math.random() * choice.length);
   return choice[randomChoice];
 }
-
 
