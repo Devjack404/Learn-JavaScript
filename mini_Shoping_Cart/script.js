a676b5b2d2018374b4af7944b4d48e97e5b60ed2
Devjack404 <juansabarofek@gmail.com>
2026-02-11 10:21:36 +0700

    membuat logika menambahkan items dan mengatur class items

--- mini_Shoping_Cart/script.js
+++ mini_Shoping_Cart/script.js
@@ -1,1 +1,23 @@
+let lemon = document.querySelector(".lemonSoda");
+let sprite = document.querySelector(".sprite");
+let orginJuice = document.querySelector(".orginJuice");
 
+let addItems = [""];
+
+lemon.addEventListener('click', () => {
+  lemon = "lemon";
+  addItems += lemon;
+  console.log(addItems);
+})
+
+sprite.addEventListener('click', () => {
+  sprite = "sprite";
+  addItems += sprite;
+  console.log(addItems);
+})
+
+orginJuice.addEventListener('click', () => {
+  orginJuice = "juice";
+  addItems += orginJuice;
+  console.log(addItems);
+})
