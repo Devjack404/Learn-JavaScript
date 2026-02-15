f3abad675b8429e96008e3e4ad8254020e494d5d
Devjack404 <juansabarofek@gmail.com>
2026-02-15 14:38:54 +0700

    fix : bug tombol kurangi quntity items < 0 (tidak ada minus)

--- mini_Shoping_Cart/cart.js
+++ mini_Shoping_Cart/cart.js
@@ -3,7 +3,7 @@ let inc_sign = document.querySelector('.inc-sign');
 let dec_sign = document.querySelector('.dec-sign');
 let remove_btn = document.querySelector('.remove-btn')
 let quantity_value = 0;
-let items_display = document.querySelector('.items-display')
+let items_display = document.querySelectorAll('.items-display')
 //function update quantity
 function update_quantity(){
   document.querySelector('.quantity_num').innerHTML = quantity_value;
@@ -17,11 +17,15 @@ inc_sign.addEventListener('click', ()=> {
 })
 
 dec_sign.addEventListener('click', ()=> {
-  quantity_value--;
+  if(quantity_value > 1){
+    quantity_value--;
+  }
   update_quantity();
   console.log(quantity_value);
 })
 
+
+//function to delete items in cart
 remove_btn.addEventListener('click', ()=> {
- items_display.style.display = "none";
+ items_display.remove();
 })
