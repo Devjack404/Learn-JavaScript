5aa3080aa1d7114303f5803000b036d0fde87009
Devjack404 <juansabarofek@gmail.com>
2026-02-15 13:53:41 +0700

    add : logic dan tombol fitur menghapus produk di cart

--- mini_Shoping_Cart/cart.js
+++ mini_Shoping_Cart/cart.js
@@ -1,12 +1,14 @@
 //logic for cart 
 let inc_sign = document.querySelector('.inc-sign');
 let dec_sign = document.querySelector('.dec-sign');
+let remove_btn = document.querySelector('.remove-btn')
 let quantity_value = 0;
-
+let items_display = document.querySelector('.items-display')
 //function update quantity
 function update_quantity(){
   document.querySelector('.quantity_num').innerHTML = quantity_value;
 }
+
 //
 inc_sign.addEventListener('click', ()=> {
   quantity_value++;
@@ -18,4 +20,8 @@ dec_sign.addEventListener('click', ()=> {
   quantity_value--;
   update_quantity();
   console.log(quantity_value);
+})
+
+remove_btn.addEventListener('click', ()=> {
+ items_display.style.display = "none";
 })
