2e82295983fbfb03482a710df5a8fcac96ac960a
Devjack404 <juansabarofek@gmail.com>
2026-02-15 14:57:11 +0700

    refactor code di cart.js

--- mini_Shoping_Cart/cart.js
+++ mini_Shoping_Cart/cart.js
@@ -1,31 +1,35 @@
-//logic for cart 
-let inc_sign = document.querySelector('.inc-sign');
-let dec_sign = document.querySelector('.dec-sign');
-let remove_btn = document.querySelector('.remove-btn')
-let quantity_value = 0;
 let items_display = document.querySelectorAll('.items-display')
-//function update quantity
-function update_quantity(){
-  document.querySelector('.quantity_num').innerHTML = quantity_value;
-}
 
-//
-inc_sign.addEventListener('click', ()=> {
-  quantity_value++;
-  update_quantity();
-  console.log(quantity_value);
-})
+//logic for cart 
+items_display.forEach(row => {  
+  let inc_sign = row.querySelector('.inc-sign');
+  let dec_sign = row.querySelector('.dec-sign');
+  let remove_btn = row.querySelector('.remove-btn');
+  let quantity_display = row.querySelector('.quantity_num');
+  let quantity_value = 1;
 
-dec_sign.addEventListener('click', ()=> {
-  if(quantity_value > 1){
-    quantity_value--;
+  //function update quantity
+  function update_quantity(){
+    quantity_display.innerHTML = quantity_value;
   }
-  update_quantity();
-  console.log(quantity_value);
-})
-
-
-//function to delete items in cart
-remove_btn.addEventListener('click', ()=> {
- items_display.remove();
-})
+  
+  //
+  inc_sign.addEventListener('click', ()=> {
+    quantity_value++;
+    update_quantity();
+    console.log(quantity_value);
+  })
+  
+  dec_sign.addEventListener('click', ()=> {
+    if(quantity_value > 1){
+      quantity_value--;
+    }
+    update_quantity();
+    console.log(quantity_value);
+  })
+   
+  //function to delete items in cart
+  remove_btn.addEventListener('click', ()=> {
+   row.remove();
+  })
+});
